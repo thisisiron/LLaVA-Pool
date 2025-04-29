521d4290e0334ffabb9dd63b4f465e64d8620d40
Eon Kim <kimiron518@gmail.com>
2025-04-29 22:57:10 +0900

    refactor: update .gitignore and enhance SaveProcessorCallback to copy configuration files after training

--- src/llavapool/train/callbacks.py
+++ src/llavapool/train/callbacks.py
@@ -1,21 +1,8 @@
-# Copyright 2024 the LlamaFactory team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import json
 import logging
 import os
 import signal
+import shutil
 import sys
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -130,6 +117,9 @@ class SaveProcessorCallback(TrainerCallback):
     def on_train_end(self, args: "TrainingArguments", state: "TrainerState", control: "TrainerControl", **kwargs):
         if args.should_save:
             self.processor.save_pretrained(args.output_dir)
+        if sys.argv[1].endswith(".json") or sys.argv[1].endswith(".yaml") or sys.argv[1].endswith(".yml"):
+            shutil.copy(os.path.abspath(sys.argv[1]), os.path.join(args.output_dir, os.path.basename(sys.argv[1])))
+
 
 
 class PissaConvertCallback(TrainerCallback):
